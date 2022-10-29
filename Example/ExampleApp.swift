347e87a0d4fb895d0de61e9d83a30772c81dcc84
ghada <ghada_alshahrani@hotmail.com>
2022-10-29 16:40:07 +0300

    Initial Commit

---
+++ Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Ghada Amer Alshahrani on 04/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
